81db131b00c600dee8181f319b12c43d7349599e
323 <323@323-3>
2022-09-08 12:17:54 +0300

    Добавлин выподающий список

--- FoodStoreApp/Data/ApplicationDbContext.cs
+++ FoodStoreApp/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace FoodStore.Data
         }
 
         public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+
     }
 }
 
---
+++ FoodStoreApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using FoodStore.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodStoreApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+        public string? Image { get; set; }
+        [Display(Name ="Тип категории")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
